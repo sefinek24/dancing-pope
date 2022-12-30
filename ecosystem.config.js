55356d5b3115299dab361e285ed2801f89892bba
Sefinek <sefinek5916@gmail.com>
2022-12-30 04:03:36 +0100

    v1.0.2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	apps: [{
-		name            : 'dancing-pope',
+		name            : 'jp2.sefinek.net',
 		script          : './index.js',
 
 		log_date_format : 'HH:mm:ss, DD.MM.YYYY',
-		error_file      : '/home/ubuntu/logs/www/dancing-pope/error.log',
-		out_file        : '/home/ubuntu/logs/www/dancing-pope/out.log',
+		error_file      : '/home/ubuntu/logs/www/jp2.sefinek.net/error.log',
+		out_file        : '/home/ubuntu/logs/www/jp2.sefinek.net/out.log',
 
 		max_restarts          : 7,
 		restart_delay         : 7000,
